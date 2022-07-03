2e4250b6083ec29f5afdb9447e5e5e6f003464e3
Maxim Kropanov <kropanov2012@gmail.com>
2022-07-03 15:54:34 +0300

    Creating routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <h1>
-            Hello hanafuda!
-        </h1>
+        <Routes>
+            <Route path="/" element={<h1>Auth</h1>}/>
+            <Route></Route>
+            <Route></Route>
+            <Route path="*" element={<h1>Not found</h1>}></Route>
+        </Routes>
     );
 }
 
