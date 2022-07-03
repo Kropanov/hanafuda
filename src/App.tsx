dddc1c37ea26963aca219e1739541316e5bdacfd
Maxim Kropanov <kropanov2012@gmail.com>
2022-07-03 14:48:46 +0300

    Last project settings before work

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <h1>
-      Hello hanafuda!
-    </h1>
-  );
+    return (
+        <h1>
+            Hello hanafuda!
+        </h1>
+    );
 }
 
 export default App;
